4a76adaebfe7757fe4c9e07e9649803095612031
teeray10 <--global>
2021-11-05 15:31:33 +0200

    Added event handlers to AddUserForm

--- practice-project-1/src/components/add-user/AddUserForm.jsx
+++ practice-project-1/src/components/add-user/AddUserForm.jsx
@@ -2,15 +2,20 @@ import Button from '../UI/Button';
 import styles from './AddUserForm.module.css';
 
 function AddUserForm(props) {
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        props.onSubmit();
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <div className={styles.input}>
                 <label>Username</label>
-                <input type="text" />
+                <input type="text" onChange={props.onUsernameChange} />
             </div>
             <div className={styles.input}>
                 <label>Age</label>
-                <input type="number" />
+                <input type="number" onChange={props.onAgeChange} />
             </div>
             <Button type="submit" text="Add User" />
         </form>
