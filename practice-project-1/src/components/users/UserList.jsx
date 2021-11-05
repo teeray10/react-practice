cc682e2ecec671597710b6f198a6bab1b54f97ac
teeray10 <--global>
2021-11-05 15:55:09 +0200

    UserList component now dynamically displays a list of users based on props

--- practice-project-1/src/components/users/UserList.jsx
+++ practice-project-1/src/components/users/UserList.jsx
@@ -1,5 +1,18 @@
+import Card from '../UI/Card';
+import User from './User';
+import styles from './UserList.module.css';
+
 function UserList(props) {
-    return null;
+    const userList = props.users.map(user => <User key={Math.random()} name={user.username} age={user.age} />);
+
+    return (
+        <Card class={styles['user-list']}>
+            <ul>
+                {props.users.length > 0 && userList}
+                {props.users.length === 0 && 'No users found.'}
+            </ul>
+        </Card>
+    );
 }
 
 export default UserList;
