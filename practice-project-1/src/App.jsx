9212775e88c5c4777ddceec50a88efdf80fb10e5
teeray10 <--global>
2021-11-05 15:33:58 +0200

    Added state and props for AddUser to App component

--- practice-project-1/src/App.jsx
+++ practice-project-1/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import AddUser from './components/add-user/AddUser';
 import UserList from './components/users/UserList';
 
 function App() {
+    const [userList, setUserList] = useState([]);
+    const addUserHandler = (user) => {
+        setUserList(prev => [...prev, user]);
+    }
+
     return (
         <div>
-            <AddUser />
+            <AddUser addUser={addUserHandler} />
             <UserList />
         </div>
     );
