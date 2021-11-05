cc74ae48ea942de81b70c4e17dfe971ea246bb12
teeray10 <--global>
2021-11-05 15:32:27 +0200

    Added state and props to AddUser

--- practice-project-1/src/components/add-user/AddUser.jsx
+++ practice-project-1/src/components/add-user/AddUser.jsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import AddUserForm from './AddUserForm';
 
 function AddUser(props) {
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
+
+    const usernameChangeHandler = ({ target }) => {
+        setUsername(target.value);
+    }
+
+    const ageChangeHandler = ({ target }) => {
+        setAge(target.value);
+    }
+    const submitHandler = () => {
+        const user = {
+            username,
+            age
+        }
+        props.addUser(user);
+    }
+
     return (
         <Card class={styles['add-user']}>
-            <AddUserForm />
+            <AddUserForm
+                onSubmit={submitHandler}
+                onUsernameChange={usernameChangeHandler}
+                onAgeChange={ageChangeHandler} />
         </Card>
     );
 }
